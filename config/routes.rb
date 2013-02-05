49d68f7386a1877c94b095747022e44a4d154d60
Onofre <onofer@gmail.com>
2013-02-05 21:45:43 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
   
+match '/signup',  to: 'users#new'
+  root to: 'static_pages#home'
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  root to: 'static_pages#home'
 
-  # The priority is based upon order of creation:
+    # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
